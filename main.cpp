b4a7d2b669c844ca499e4da5a4dd3c269c432d75
zero <shashwatjain8@gmail.com>
2018-10-04 22:10:54 +0530

    main function edit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+
 #include "mainwindow.h"
 #include <QApplication>
-
+#include<iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
+    cout<<"Entry Point";
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
